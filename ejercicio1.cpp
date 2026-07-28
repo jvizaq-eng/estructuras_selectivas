882fd3501a56d7a06b54f075f0018c35846fb9fc
Jennifer <jvizaq@unjbg.edu.pe>
2026-07-27 20:10:50 -0500

    Mejora los mensajes de entrada y salida de consola

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,17 +3,19 @@ using namespace std;
 
 int main() {
     int dia;
+    cout << "=== DETERMINAR DIA DE LA SEMANA ===" << endl;
     cout << "Ingrese un numero del 1 al 7: ";
     cin >> dia;
+
     switch (dia) {
-        case 1: cout << "Lunes"; break;
-        case 2: cout << "Martes"; break;
-        case 3: cout << "Miercoles"; break;
-        case 4: cout << "Jueves"; break;
-        case 5: cout << "Viernes"; break;
-        case 6: cout << "Sabado"; break;
-        case 7: cout << "Domingo"; break;
-        default: cout << "Numero fuera de rango";
+        case 1: cout << "El dia es: Lunes" << endl; break;
+        case 2: cout << "El dia es: Martes" << endl; break;
+        case 3: cout << "El dia es: Miercoles" << endl; break;
+        case 4: cout << "El dia es: Jueves" << endl; break;
+        case 5: cout << "El dia es: Viernes" << endl; break;
+        case 6: cout << "El dia es: Sabado" << endl; break;
+        case 7: cout << "El dia es: Domingo" << endl; break;
+        default: cout << "Error: Numero fuera de rango (1-7)" << endl;
     }
     return 0;
 }
